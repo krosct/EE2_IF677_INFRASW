790b12c7fc7bac52074371545f4db804170572c7
krosct <krosct@gmail.com>
2024-06-19 22:44:56 -0300

    class conta and depositar method

--- EE2 Problema 1/Problema1SistemaBancario/src/Banco.java
+++ EE2 Problema 1/Problema1SistemaBancario/src/Banco.java
@@ -1,33 +1,60 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Conta {
+    private double saldo;
+    private Lock lock = new ReentrantLock();
+    private Condition saldoSuficiente = lock.newCondition();
+
+    public Conta(double saldoInicial) {
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double quantia) {
+        lock.lock();
+        try {
+            saldo += quantia;
+            System.out.println(Thread.currentThread().getName() + " (+++) R$" + quantia + ". Saldo: R$" + saldo + ".");
+            saldoSuficiente.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
 
 class Pessoa implements Runnable {
+    private Conta conta;
     private boolean tipoOperacao; // true para depositar, false para sacar
     private double quantia;
 
-    public Pessoa(boolean tipoOperacao, double quantia) {
+    public Pessoa(Conta conta, boolean tipoOperacao, double quantia) {
+        this.conta = conta;
         this.tipoOperacao = tipoOperacao;
         this.quantia = quantia;
     }
 
     @Override
     public void run() {
-        System.out.println("Eu sou " + Thread.currentThread().getName() + ". Tipo de operação: " + tipoOperacao + ". Quantia: " + quantia);
+        conta.depositar(quantia);
     }
 }
 
 public class Banco {
     public static void main(String[] args) {
+        Conta contaCompartilhada = new Conta(1000);
         List<Thread> pessoas = new ArrayList<>();
 
-        pessoas.add(new Thread(new Pessoa(true, 277), "Pessoa1"));
-        pessoas.add(new Thread(new Pessoa(false, 6000.0), "Pessoa5"));
-        pessoas.add(new Thread(new Pessoa(true, 1500.0), "Pessoa2"));
-        pessoas.add(new Thread(new Pessoa(false, 6765.0), "Pessoa6"));
-        pessoas.add(new Thread(new Pessoa(false, 3456.0), "Pessoa7"));
-        pessoas.add(new Thread(new Pessoa(true, 9988.0), "Pessoa3"));
-        pessoas.add(new Thread(new Pessoa(true, 7299.0), "Pessoa4"));
-        pessoas.add(new Thread(new Pessoa(false, 211.0), "Pessoa8"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, true, 277), "Pessoa1"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, false, 6000.0), "Pessoa5"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, true, 1500.0), "Pessoa2"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, false, 6765.0), "Pessoa6"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, false, 3456.0), "Pessoa7"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, true, 9988.0), "Pessoa3"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, true, 7299.0), "Pessoa4"));
+        pessoas.add(new Thread(new Pessoa(contaCompartilhada, false, 211.0), "Pessoa8"));
 
         // Inicia todas as threads
         for (Thread t: pessoas) {
